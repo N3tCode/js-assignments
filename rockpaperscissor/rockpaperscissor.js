c33c9cb6db4c96abbdbca4228e9fd379f475c7f7
N3tCode <codyhub@gmail.com>
2022-12-09 14:50:13 -0500

    Add pseudocode

---
+++ rockpaperscissor/rockpaperscissor.js
@@ -0,0 +1,4 @@
+/* user inputs a choice for the game rock, paper, scissor
+/* computer randomly chooses rock, paper, or scissor
+/* a score is given to each player after each round
+/* after 5 rounds a winner is declared

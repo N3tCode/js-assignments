eeb8a01c04dace2f5dae4a266ab7cd7f07f33d14
N3tCode <codyhub@gmail.com>
2022-12-12 14:41:17 -0500

    Establish working random computer choice

--- rockpaperscissor/rockpaperscissor.js
+++ rockpaperscissor/rockpaperscissor.js
@@ -1,4 +1,14 @@
-/* user inputs a choice for the game rock, paper, scissor
-/* computer randomly chooses rock, paper, or scissor
-/* a score is given to each player after each round
-/* after 5 rounds a winner is declared
+/* user inputs a choice for the game rock, paper, scissor */
+/* computer randomly chooses rock, paper, or scissor */
+
+let randomChoice;
+
+function getComputerChoice(randomChoice) {
+    const choices = ['Rock', 'Paper', 'Scissor'];
+    return randomChoice = choices[Math.floor(Math.random() * choices.length)];
+}
+
+console.log(getComputerChoice(randomChoice));
+
+/* a score is given to each player after each round */
+/* after 5 rounds a winner is declared */

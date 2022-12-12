103cec6ed01b9d36f1ba2d8780ea8785559f3c9d
N3tCode <codyhub@gmail.com>
2022-12-12 16:01:19 -0500

    Establish working play round code and rearrange getComputerChoice code

--- rockpaperscissor/rockpaperscissor.js
+++ rockpaperscissor/rockpaperscissor.js
@@ -1,14 +1,37 @@
-/* user inputs a choice for the game rock, paper, scissor */
 /* computer randomly chooses rock, paper, or scissor */
 
-let randomChoice;
-
 function getComputerChoice(randomChoice) {
-    const choices = ['Rock', 'Paper', 'Scissor'];
+    const choices = ['rock', 'paper', 'scissor'];
     return randomChoice = choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice(randomChoice));
+let randomChoice;
+
+/* user inputs a choice for the game rock, paper, scissor */
+
+function playRound(playerSelection, computerSelection) {
+    let lowerCasePlayerSelection = playerSelection.toLowerCase();
+    console.log(lowerCasePlayerSelection)
+    if (lowerCasePlayerSelection === 'rock' && computerSelection === 'paper')
+        return 'You LOSE! Paper beats Rock :(';
+        else if (lowerCasePlayerSelection === 'rock' && computerSelection === 'scissor')
+        return 'You WIN! Rock beats Scissor :)';
+        else if (lowerCasePlayerSelection === 'paper' && computerSelection === 'rock')
+        return 'You WIN! Paper beats Rock :)';
+        else if (lowerCasePlayerSelection === 'paper' && computerSelection === 'scissor')
+        return 'You LOSE! Scissor beats Paper :(';
+        else if (lowerCasePlayerSelection === 'scissor' && computerSelection === 'rock')
+        return 'You LOSE! Rock beats Scissor :(';
+        else if (lowerCasePlayerSelection === 'scissor' && computerSelection === 'paper')
+        return 'You WIN! Scissor beats Paper :)';
+        else if (lowerCasePlayerSelection === computerSelection)
+        return 'Oh man a TIE! Try again stir fry!! :S';
+  }
+   
+  const playerSelection = 'SCISSOR';
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(computerSelection);
 
 /* a score is given to each player after each round */
 /* after 5 rounds a winner is declared */

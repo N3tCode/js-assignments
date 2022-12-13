49ec1e8c2095b30811533ecd834e5c5486b4a353
N3tCode <codyhub@gmail.com>
2022-12-13 17:10:28 -0500

    Add a global scoreboard, fix the game loop with a declared winner at the end

--- rockpaperscissor/rockpaperscissor.js
+++ rockpaperscissor/rockpaperscissor.js
@@ -1,28 +1,29 @@
-// the playRound loops until a winner of 5 rounds is determined
+// score boards
+
+let playerScore = 0;
+let computerScore = 0;
+
+// the playRound loops until a best out of 5 winner is determined
 // if player wins, the player is awarded one point which is
 // stored in the playerScore variable
 // if computer wins, the player is awarded one point which is
 // stored in the computerScore variable
-console.log(game());
+// after 5 rounds a winner is declared
+
+game();
 
  function game() {
     for (let i = 0; i < 5; i++) {
         console.log(playRound());
-        let playerScore = 0;
-        if (playerWin) {
-            ++playerScore;
-            if (playerScore = 5) {
-                return 'You DEFEATED the computer!';
-            }
-        }
-        let computerScore = 0;
-        if (computerWin) {
-            ++computerScore;
-            if (computerScore = 5) {
-                return 'You have been DEFEATED by the computer!';
-            }
-        }
      }
+
+    if (playerScore > computerScore) {
+        return console.log('Game Over... You WIN!!!');
+    }   else if (playerScore < computerScore) {
+        return console.log('Game Over... You LOSE.');
+    }   else {
+        return console.log('Game Over... It\'s a TIE lol');
+    }
 }
 
 // computer randomly chooses rock, paper, or scissor
@@ -33,36 +34,29 @@ function getComputerChoice() {
 }
 
 // user inputs a choice for the game rock, paper, scissor
-
-//let playerSelection = prompt('Enter your choice.. Rock, Paper, Scissor:','');
-
-// a round is played between the player and computer with a win or lose result returned */
+// a round is played between the player and computer with a win or lose result returned
+// and a point added to the round winner's score
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Enter your choice.. Rock, Paper, Scissor:','');
+    playerSelection = prompt('Enter your choice.. Rock, Paper, Scissor:','').toLowerCase();
     computerSelection = getComputerChoice();
-    let lowerCasePlayerSelection = playerSelection.toLowerCase();
-    console.log(lowerCasePlayerSelection);
+    console.log(playerSelection);
     console.log(computerSelection);
     let computerWin = (`You LOSE! ${computerSelection} beats ${playerSelection}`);
-    let playerWin = (`You WIN! ${playerSelection} beats ${computerSelection}`)
-    if (lowerCasePlayerSelection === 'rock' && computerSelection === 'paper') {
-        return computerWin;
-    }   else if (lowerCasePlayerSelection === 'rock' && computerSelection === 'scissor') {
-        return playerWin;
-    }   else if (lowerCasePlayerSelection === 'paper' && computerSelection === 'rock') {
-        return playerWin;
-    }   else if (lowerCasePlayerSelection === 'paper' && computerSelection === 'scissor') {
-        return computerWin;
-    }   else if (lowerCasePlayerSelection === 'scissor' && computerSelection === 'rock') {
-        return computerWin;
-    }   else if (lowerCasePlayerSelection === 'scissor' && computerSelection === 'paper') {
-        return playerWin;
-    }   else if (lowerCasePlayerSelection === computerSelection) {
+    let playerWin = (`You WIN! ${playerSelection} beats ${computerSelection}`);
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return (computerWin + ++computerScore);
+    }   else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return (playerWin + ++playerScore);
+    }   else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return (playerWin + ++playerScore);
+    }   else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        return (computerWin + ++computerScore);
+    }   else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        return (computerWin + ++computerScore);
+    }   else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        return (playerWin + ++playerScore);
+    }   else if (playerSelection === computerSelection) {
         return 'Oh man a TIE! Try again stir fry!! :S';
     }   else alert('What the chocolate chip! That\'s not a choice for this game. Please try again friend. :)');
   }
-
-//const computerSelection = getComputerChoice();
-//console.log(playRound(playerSelection, computerSelection));
-//console.log(computerSelection);
